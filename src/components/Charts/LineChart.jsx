58f1e07268edc8a9fb587fd69247ee335c0493a3
ilkay kesici <ilkayksc@gmail.com>
2022-06-21 17:54:36 +0300

    Line and area chart completed

--- src/components/Charts/LineChart.jsx
+++ src/components/Charts/LineChart.jsx
@@ -1,9 +1,45 @@
-import React from 'react'
-
+import React from "react";
+import { Header } from "../../components";
+import {
+  ChartComponent,
+  SeriesCollectionDirective,
+  SeriesDirective,
+  Inject,
+  DateTime,
+  Legend,
+  Tooltip,
+  LineSeries,
+} from "@syncfusion/ej2-react-charts";
+import {
+  lineCustomSeries,
+  LinePrimaryXAxis,
+  LinePrimaryYAxis,
+} from "../../data/dummy";
+import { useStateContext } from "../../contexts/ContextProvider";
 const LineChart = () => {
+  const {currentMode} = useStateContext();
   return (
-    <div>LineChart</div>
-  )
-}
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Color Picker" />
+    <ChartComponent
+      id="line-chart"
+      height="420px"
+      primaryXAxis={LinePrimaryXAxis}
+      primaryYAxis={LinePrimaryYAxis}
+      chartArea={{border:{width:0}}}
+      tooltip={{enable:true}}
+      background={currentMode ==='Dark'? '#33373E':'#fff'}
+      
+    >
+      <Inject services={[LineSeries, DateTime, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {lineCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+    </div>
+  );
+};
 
-export default LineChart
+export default LineChart;
